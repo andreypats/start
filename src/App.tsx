2fe633f0b16380986d18fa4dcc8a519f5f84d1aa
andreypats <andrey.it.work@gmail.com>
2022-07-11 23:59:33 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
+
+function App() {    // компонента - функция, возвращающая jsx
+    console.log("App rendering");
+    //debugger
+    // полезное что-то
+    // обязана что-то вернуть (jsx)
+    return (
+        <div>
+            <PageTitle title={"This is APP component"}/>
+            <PageTitle title={"My friends"}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion titleValue={"Menu"} collapsed={false}/>
+            <Accordion titleValue={"Users"} collapsed={true}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
+}
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log("PageTitle rendering");
+    return (
+        <h1>
+            {props.title}
+        </h1>
+    )
+}
+
+export default App;
